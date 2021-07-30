51031dc4382e5263f74f6760a9a484dab97ff0a9
louist103 <35883445+louist103@users.noreply.github.com>
2021-07-30 12:17:19 -0400

    Update release-linux32.sh

--- release-linux32.sh
+++ release-linux32.sh
@@ -1,10 +1,10 @@
 # build decoder functions
-gcc -m32 -DNDEBUG -s -Ofast -flto -lm -c -Wall src/decoder/*.c
+gcc -m32 -DNDEBUG -s -Ofast -flto -lm -c -Wall -march=native -mtune=native src/decoder/*.c
 mkdir -p o
 mv *.o o
 
 # build everything else
-gcc -m32 -o z64decompress -DNDEBUG src/*.c o/*.o -Wall -Wextra -s -Os -flto
+gcc -m32 -o z64decompress -DNDEBUG src/*.c o/*.o -Wall -Wextra -s -Os -flto -march=native -mtune=native
 
 # move to bin directory
 mkdir -p bin/linux32
