728f97eba151c32a31a9752c308bad3004379d70
louist103 <35883445+louist103@users.noreply.github.com>
2021-07-30 12:17:41 -0400

    Update release-linux.sh

--- release-linux.sh
+++ release-linux.sh
@@ -1,10 +1,10 @@
 # build decoder functions
-gcc -DNDEBUG -s -Ofast -flto -lm -c -Wall -Wextra src/decoder/*.c
+gcc -DNDEBUG -s -Ofast -flto -lm -c -Wall -Wextra -march=native -mtune=native src/decoder/*.c
 mkdir -p o
 mv *.o o
 
 # build everything else
-gcc -o z64decompress -DNDEBUG src/*.c o/*.o -Wall -Wextra -s -Os -flto
+gcc -o z64decompress -DNDEBUG src/*.c o/*.o -Wall -Wextra -s -Os -flto -march=native -mtune=native
 
 # move to bin directory
 mkdir -p bin/linux64
